96de5ece74bfb892d7cbfac98e9651599b397dd8
Ally Juma Said <mahadsaid95@icloud.com>
2024-10-01 22:03:18 -0700

    finance report

---
+++ app/financereport.tsx
@@ -0,0 +1,6 @@
+// financial report
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Layout, Menu, Breadcrumb, Table, Button } from 'antd';
+import { UserOutlined, LaptopOutlined, NotificationOutlined } from '@ant-design/icons';
+import { ColumnProps } from 'antd/es/table';
